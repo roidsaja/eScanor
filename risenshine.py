831b25f5f37da9cc2a943bdf932619274ff0d9f4
Roid Maulana <roidthebeast@gmail.com>
2019-11-02 15:03:14 +0700

    update risenshine.py

--- risenshine.py
+++ risenshine.py
@@ -1,25 +1,4 @@
 #!/usr/bin/python
-# 
-# PiTextReader - Raspberry Pi Printed Text-to-Speech Reader
-#
-# Allows sight impaired person to have printed text read using
-# OCR and text-to-speech.
-#
-# Normally run by pi crontab at bootup
-# Turn off by commenting out @reboot... using $ crontab -e; sudo reboot
-# Manually run using $ python pitextreader.py
-#
-# This is a simplistic (i.e. not pretty) python program
-# Just runs cmd-line pgms raspistill, tesseract-ocr, flite to do all the work
-#
-# Version 1.0 2018.02.10 - initial release - rgrokett
-# v1.1 - added some text cleanup to improve reading
-# v1.2 - removed tabs
-#
-# http://kd.grokett.com/
-#
-# License: GPLv3, see: www.gnu.org/licenses/gpl-3.0.html
-#
  
 import RPi.GPIO as GPIO
 import os, sys
@@ -40,8 +19,6 @@ CAMERA  = "raspistill -cfx 128:128 --awb auto -rot 180 -t 500 -o /tmp/image.jpg"
 
 # GPIO BUTTONS
 BTN1    = 24    # The button!
-#LED     = 18    # The button's LED!
-
 
 ### FUNCTIONS
 # Thread controls for background processing
@@ -62,14 +39,6 @@ class RaspberryThread(threading.Thread):
     def stop(self):
         self.running = False 
 
-# LED ON/OFF
-# def led(val):   
-   # logger.info('led('+str(val)+')') 
-   # if val:
-      # GPIO.output(LED,GPIO.HIGH)
-   # else:
-      # GPIO.output(LED,GPIO.LOW)
-    
 # PLAY SOUND
 def sound(val): # Play a sound
     logger.info('sound()') 
@@ -132,7 +101,6 @@ def stopTTS():
 # GRAB IMAGE AND CONVERT
 def getData():
     logger.info('getData()') 
-   # led(0) # Turn off Button LED
 
     # Take photo
     sound(SOUNDS+"camera-shutter.wav")
@@ -178,7 +146,6 @@ try:
     GPIO.setwarnings (False)
      
     GPIO.setup(BTN1, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
-   # GPIO.setup(LED, GPIO.OUT) 
     
     # Threaded audio player
     #rt = RaspberryThread( function = repeatTTS ) # Repeat Speak text
@@ -186,7 +153,6 @@ try:
     
     volume(VOLUME)
     speak("OK, ready")
-   # led(1)
     
     while True:
         if GPIO.input(BTN1) == GPIO.LOW:
@@ -194,7 +160,6 @@ try:
             getData()
             rt.stop()
             rt = RaspberryThread( function = stopTTS ) # Stop Speaking text
-           # led(1)
             time.sleep(0.5)  
             speak("OK, ready")
         time.sleep(0.2)  
